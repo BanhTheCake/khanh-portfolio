7b434b58794f20671a69da7bee1b5422f96b9a3f
banhTheCake <binhanh080103@gmail.com>
2026-06-29 13:27:23 +0700

    Six

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <div>
     <div className="flex h-screen flex-col items-center justify-center relative">
-    <div className="absolute top-0 left-0 w-full h-full bg-[url('Images/background_1.png')] object-cover object-center">
+    <div className="absolute top-0 left-0 w-full h-full bg-[url('/Images/background_1.png')] bg-cover bg-center">
     <video className="w-full h-full object-cover object-center" src="Videos/Cover.mp4" autoPlay loop muted></video>
     <div className="absolute top-0 left-0 w-full h-full opacity-80 bg-linear-to-t from-black to-transparent"></div>
     </div>
